690c1bd44af27c8c1f774a4443ab3e0c4867e6d6
Eduardo Cuervo <eacuervo@gmail.com>
2012-03-14 03:05:09 -0400

    Generated deltas are transformed to YUV420 and encoded using libx264. NALs are not written yet

--- kahawai.h
+++ kahawai.h
@@ -1,6 +1,6 @@
 //Master renders hi quality, slave renders low quality
 enum KAHAWAI_MODE { Master=0, Slave=1};
-
+#define MAPPING_SIZE (1024*768*3)+18
 
 extern KAHAWAI_MODE kahawaiProcessId;
 extern HANDLE kahawaiMutex;
