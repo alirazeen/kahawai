406a1b8a709ea91d727d071bca3bd8fef1c8a975
Ali Razeen <alirazeen@alirazeen.com>
2013-04-11 10:03:24 -0400

    Implement general structure of the client encoder

--- IframeClientEncoder.cpp
+++ IframeClientEncoder.cpp
@@ -1,7 +1,7 @@
 #include "kahawaiBase.h"
 #ifdef KAHAWAI
 #include "IframeClientEncoder.h"
-
+#include "X264Encoder.h"
 
 IFrameClientEncoder::IFrameClientEncoder(void)
 {
@@ -11,4 +11,32 @@ IFrameClientEncoder::IFrameClientEncoder(void)
 IFrameClientEncoder::~IFrameClientEncoder(void)
 {
 }
+
+bool IFrameClientEncoder::Initialize(ConfigReader* configReader)
+{
+	// Read all encoder settings
+	_height = _configReader->ReadIntegerValue(CONFIG_RESOLUTION,CONFIG_HEIGHT);
+	_width = _configReader->ReadIntegerValue(CONFIG_RESOLUTION,CONFIG_WIDTH);
+	_crf = _configReader->ReadIntegerValue(CONFIG_ENCODER,CONFIG_CRF);
+	_preset = _configReader->ReadIntegerValue(CONFIG_ENCODER,CONFIG_ENCODER_LEVEL);
+	_gop = configReader->ReadIntegerValue(CONFIG_IFRAME,CONFIG_GOP_SIZE);
+
+	//Initialize encoder
+	_encoder = new X264Encoder(_height, _width, _fps, _crf, _preset, _gop);
+
+	return (_encoder != NULL);
+}
+
+int IFrameClientEncoder::Encode(void** transformedFrame)
+{
+	// TODO: Implement this
+	return -1;
+}
+
+bool IFrameClientEncoder::Send(void** compressedFrame, int frameSize)
+{
+	// TODO: Implement this
+	return false;
+}
+
 #endif
--- IframeClientEncoder.h
+++ IframeClientEncoder.h
@@ -1,10 +1,27 @@
 #pragma once
 #include "kahawaiserver.h"
-class IFrameClientEncoder :
-	public KahawaiServer
+class IFrameClientEncoder
 {
 public:
 	IFrameClientEncoder(void);
 	~IFrameClientEncoder(void);
+
+
+	bool				Initialize(ConfigReader* configReader);
+
+	int					Encode(void** transformedFrame);
+	bool				Send(void** compressedFrame, int frameSize);
+
+private:
+
+	// Encoder settings
+	int					_height;
+	int					_width;
+	static const int	_fps = TARGET_FPS;
+	int					_crf;
+	int					_preset;
+	int					_gop;
+
+	VideoEncoder*		_encoder;
 };
 
